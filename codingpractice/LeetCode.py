f983091ac24abe76121ec1b8ca1fecb8b5a119fc
jonathan louie <jonathan.louie@hotmail.com>
2023-07-07 09:42:40 -0700

    twosum

--- codingpractice/LeetCode.py
+++ codingpractice/LeetCode.py
@@ -1,11 +1,15 @@
+from typing import List
 
-def increment(x):
-    print("increment")
-    return x + 1
+class LeetCode():
+    def __init__(self) -> None:
+        pass
+    def increment(x):
+        print("increment")
+        return x + 1
 
-def decrement(x):
-    print("decrement")
-    return x - 1
+    def decrement(x):
+        print("decrement")
+        return x - 1
 
-def test():
-    assert increment(3) == 5
+    def twoSum1(self, nums: List[int], target: int) -> List[int]:
+        return nums
--- codingpractice/LeetCode_test.py
+++ codingpractice/LeetCode_test.py
@@ -3,9 +3,17 @@
 import LeetCode
 import unittest
 
-class LeetCode_test(unittest.TestCase):
+class LeetCode_test(unittest.TestCase, LeetCode.LeetCode):
+    
+    def __init__(self, methodName: str = "runTest") -> None:
+        super().__init__(methodName)
     def test_increment(self):
-        assert LeetCode.increment(3) == 4
+        assert LeetCode.LeetCode.increment(3) == 4
     def test_descrement(self):
-        assert LeetCode.decrement(3) == 2
+        assert LeetCode.LeetCode.decrement(3) == 2
+    def test_1twoSum(self):
+        nums = {1,2,3,4}
+        target = 5
+        assert LeetCode.LeetCode.twoSum1(LeetCode, nums, target) == {1,2,3,4}
+    
         
